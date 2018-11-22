62caf71296cc51ad1d497501f6073ddbbfc60964
thaks9204 <45157938+thaks9204@users.noreply.github.com>
2018-11-22 18:49:24 +0900

    git ignore add

--- src/kr/util/ConnectionFactory.java
+++ src/kr/util/ConnectionFactory.java
@@ -11,7 +11,7 @@ public class ConnectionFactory {
 		try {
 			Class.forName("oracle.jdbc.driver.OracleDriver");
 			
-			String url = "jdbc:oracle:thin:@192.168.0.37:1521:xe";
+			String url = "jdbc:oracle:thin:@localhost:1521:xe";
 			String user = "hr";
 			String password = "hr";
 			conn = DriverManager.getConnection(url, user, password);
